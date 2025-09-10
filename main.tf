4514d310ab83badc54713158ea887cf03cdf3893
Augustine Adjei <78450734+Avstxn98@users.noreply.github.com>
2025-09-10 18:12:17 +0100

    create new dev vpc

--- main.tf
+++ main.tf
@@ -20,6 +20,8 @@ resource "aws_instance" "blog" {
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
+  subnet_id = module.blog_vpc.public_subnets[0]
+
   tags = {
     Name = "HelloWorld"
   }
@@ -29,13 +31,32 @@ data "aws_vpc" "default"{
   default = true
 }
 
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 module "blog_sg"{
   source = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
   name = "blog"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id
   
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
