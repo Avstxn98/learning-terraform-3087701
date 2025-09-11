c9038f4a92d4ec8adf1793a67fed37bd31d625ef
Augustine Adjei <78450734+Avstxn98@users.noreply.github.com>
2025-09-11 18:04:04 +0100

    c3

--- main.tf
+++ main.tf
@@ -50,14 +50,7 @@ module "alb" {
     }
   }
 
-  http_tcp_listeners = [
-    {
-      port = 80
-      protocol = "HTTP"
-      target_group_index = 0
-    }
-  ]
-
+  
   tags = {
     Environment = "dev"
     
